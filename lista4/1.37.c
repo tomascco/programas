4e46e412388dc79bb8128d0ba14ae8d641c9be72
Tomas Coelho <tomascoelho6@gmail.com>
2018-03-30 14:20:46 -0300

    desisto, programa incompleto e é só isso mesmo.

---
+++ lista4/1.37.c
@@ -0,0 +1,30 @@
+/*
+Autor: Tomás de Carvalho Coelho, Eng comp, 418391
+
+ALGORITMO
+
+FIM_ALGORITMO
+*/
+#include <stdio.h>
+#include <math.h>
+#define MASCULINO 1
+#define FEMININO 2
+
+
+int main() {
+  double alturas_M, altura_atual, alturas_total;
+  int sexo_atual, num_M;
+  for (int i = 1; i <= 50; i++) {
+    printf("Ficha %d: Insira o sexo e a altura da pessoa separadas por espaço: ", i);
+    scanf("%d", &sexo_atual);
+    scanf("%lf", &altura_atual);
+    if (sexo_atual == MASCULINO)
+
+  }
+
+
+
+
+
+  return 0;
+}
